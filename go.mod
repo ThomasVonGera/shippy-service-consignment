5e15d5bca57c2676a34118a3e49128feba47137b
ThomasVonGera <32611351+ThomasVonGera@users.noreply.github.com>
2020-08-30 16:29:17 +0200

    refactoring

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/golang/protobuf v1.4.2
 	github.com/google/go-cmp v0.5.0 // indirect
 	github.com/micro/go-micro/v2 v2.9.1
+	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.5.1 // indirect
 	go.mongodb.org/mongo-driver v1.4.0
 	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
--- main.go
+++ main.go
@@ -5,67 +5,44 @@ import (
 	"log"
 
 	pb "github.com/ThomasVonGera/shippy-service-consignment/proto/consignment"
+	vesselProto "gitbug.com/ThomasVonGera/shippy/shippy-service-vessel/proto/vessel"
 	"github.com/micro/go-micro/v2"
 )
 
-const serviceName = "shippy-.service.Consignment"
+const (
+		serviceName = "shippy-.service.Consignment"
+		defaultHost = "datastore:27017"
+)
 
-type repositoryinterface interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
-}
-type Repository struct {
-	consignments []*pb.Consignment
-}
-
-// Neue Consignment erstellen
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	updated := append(repo.consignments, consignment)
-	repo.consignments = updated
-	return consignment, nil
-}
-
-func (repo *Repository) GetAll() []*pb.Consignment {
-	return repo.consignments
-}
-
-type consignmentService struct {
-	repo repositoryinterface
-}
-
-func (s *consignmentService) CreateConsignment(ctx context.Context, request *pb.Consignment, res *pb.Response) error {
-	consignment, err := s.repo.Create(request)
-	if err != nil {
-		return err
-	}
-	res.Created = true
-	res.Consignment = consignment
-	return nil
-}
-
-func (s *consignmentService) GetConsignments(ctx context.Context, request *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
-	res.Consignments = consignments
-	return nil
-}
 
 func main() {
-	repo := &Repository{}
-
 	service := micro.NewService(
-		micro.Name(serviceName),
+		micro.Name(serviceName)
 	)
 
-	// initialisieren
 	service.Init()
 
-	//registrieren
-	if err := pb.RegisterShippingServiceHandler(service.Server(), &consignmentService{repo}); err != nil {
+	uri := os.Getenv("DB_HOST")
+	if uri == "" {
+		uri = defaultHost
+	}
+
+	client,err := CreateClient(context.Background(), uri, 0)
+	if err != nil {
 		log.Panic(err)
 	}
 
-	//server ausführen
+	defer client.Disconnect(context.Background())
+
+	consignmentCollection := client.Database("shippy").Collection("consignments")
+
+	vesselClient := vesselProto.NewVesselService("shippy.service.client", service.Client())
+
+	hilfsHandler := &handler{repository, vesselClient}
+
+	pb.RegisterShippingServiceHandler(service.Server(), hilfsHandler)
+
 	if err := service.Run(); err != nil {
-		log.Panic(err)
+		fmt.Println(err)
 	}
 }
